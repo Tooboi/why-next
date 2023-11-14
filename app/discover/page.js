7da4cdd295fbe7e9819f36084c46a15d938e88fe
Tooboi <japica123@gmail.com>
2023-11-14 08:57:39 -0500

    ACE

--- app/discover/page.js
+++ app/discover/page.js
@@ -4,7 +4,8 @@ const DiscoverPage = () => {
   return (
     <>
         <h1 className='text-2xl font-bold mb-5'>Discover</h1>
-        <p className='mb-5'>welcome to the discovery page</p>
+        <p className='mb-5'>Health Coaching for you</p>
+        <p>ACE Certified Health Coach</p>
     </>
   )
 }

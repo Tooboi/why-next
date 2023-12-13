b1109eaeebe3197dab9d5cb4db84ea4f3733a672
Tooboi <japica123@gmail.com>
2023-12-12 21:45:28 -0500

    Fixing apostrophes

--- app/about/page.js
+++ app/about/page.js
@@ -4,13 +4,13 @@ const AboutPage = () => {
   return (
     <>
         <h1 className='text-4xl font-bold mb-12 text-center'>Welcome</h1>
-        <h1 className='text-2xl font-bold mb-12 text-center'>I'm so excited that you are here!</h1>
+        <h1 className='text-2xl font-bold mb-12 text-center'>I&apos;m so excited that you are here!</h1>
         <p className='text-xl mb-8 text-center'>My name is Tammy. I am an ACE certified Health and Wellness coach and I have been a healthcare professional for 30 + years.</p>
         <p className='text-xl mb-8 text-center'>I work closely with patients living with cancer, aiding them in becoming well again through radiation therapy. Most recently, I have become certified in Cryoskin slimming, toning, facials and pain relief. I am extremely passionate about my work.</p>
         <p className='text-xl mb-8 text-center'>In my free time you can find me at our local fitness center taking a class or training for an upcoming event. I also love spending time with my family and friends, baking, and cooking. I love the outdoors too.</p>
         <p className='text-xl mb-8 text-center'>As a mother of three, I have always been conscious about my health, trying my best to follow a healthy lifestyle and encourage my loved ones to do the same by eating well, staying active and being optimistic about life.</p>
         <p className='text-xl mb-8 text-center'>I would be honored to walk side by side with you on your path to wellness.</p>
-        <p className='text-xl font-bold mb-8 text-center'>Let's discover the strengths you already have and put them in use to get you where you want to go.</p>
+        <p className='text-xl font-bold mb-8 text-center'>Let&apos;s discover the strengths you already have and put them in use to get you where you want to go.</p>
     </>
   )
 }

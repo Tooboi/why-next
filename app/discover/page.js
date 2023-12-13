e1e6384bcaf01372e583477c9a1c69db353af186
Tooboi <japica123@gmail.com>
2023-12-12 21:34:06 -0500

    Adding text

--- app/discover/page.js
+++ app/discover/page.js
@@ -3,9 +3,9 @@ import React from 'react'
 const DiscoverPage = () => {
   return (
     <>
-        <h1 className='text-2xl font-bold mb-5'>Discover</h1>
-        <p className='mb-5'>Health Coaching for you</p>
-        <p>ACE Certified Health Coach</p>
+        <h1 className='text-2xl font-bold mb-5 text-center'>Discover</h1>
+        <p className='text-xl mb-5 text-center'>Health Coaching</p>
+        <p className='text-xl mb-5 text-center'>Cryoskin Slimming, Toning, Facials, Pain Relief</p>
     </>
   )
 }
